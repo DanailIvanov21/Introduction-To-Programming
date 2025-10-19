f537d95c25223e807482c9d161a8ccde2f517f8f
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2025-10-19 14:41:49 +0300

    Update Practicum_Solutions.cpp

--- Week 2/Practicum_Solutions.cpp
+++ Week 2/Practicum_Solutions.cpp
@@ -56,7 +56,7 @@
 	std::cout << "Enter a year: ";
 	std::cin >> year;
 
-	if (year % 4 == 0 && ((year % 100 != 0) || year % 400 == 0)) {
+	if ((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0) {
 		std::cout << year << " is a leap year!" << std::endl;
 	}
 	else {
